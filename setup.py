f6f7e016aa5eea26a7b805d58bc0981805a4f7f0
Jonatan Heyman <jonatan@heyman.info>
2014-07-14 12:36:24 +0200

    Added link to overview page. Bumped version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from io import open
 
 setup(
     name='django-mstats',
-    version='0.1.1',
+    version='0.1.2',
     description='Simple, re-usable, stateless Django app for visualizing and browsing '
                 'statistics, mainly based on your existing Django models.',
     long_description=open('README.rst', encoding='utf-8').read(),
